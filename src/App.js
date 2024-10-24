c4faa5a479fe2a0a8df691a778fd49fe463c23cd
usama2333 <usama.wizz@gmail.com>
2024-10-24 17:06:33 +0500

    use Context in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { createContext, useState } from "react";
 import UseEffectDemo from "./components/UseEffectDemo";
 import UseStateDemo from "./components/UseStateDemo";
+import FunctionContextComponent from "./components/FunctionContextComponent";
+
+// this how to create contentex
+export const ThemeContext = createContext()
 
 export default function App() {
+
+  const [darkTheme, setDarkTheme] = useState(true);
+
+
+  function toggleTheme() {
+    setDarkTheme(prevDarkTheme => !prevDarkTheme)
+  }
    
   return (
       <>
       {/* <UseStateDemo/> */}
-      <UseEffectDemo/>
+      {/* <UseEffectDemo/> */}
+{/* here provide the theme with some value that is avaliable in all child 
+that are call inside the provider */}
+      <ThemeContext.Provider value={darkTheme}>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <FunctionContextComponent/>
+      </ThemeContext.Provider>
         
       </>
   )
